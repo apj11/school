76653f1cd95cc7a79e4d25109b8ab43c48b7c581
Avishek <parajuliavishek@gmail.com>
2020-02-09 10:52:37 +0545

    send us your query

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -327,18 +327,20 @@
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2">
                         <div class="subscribe-content section-title text-center">
-                            <h2>subscribe our newsletter</h2>
+                            <h2>send us your query</h2>
                             <p>I must explain to you how all this mistaken idea </p>
                         </div>
                         <div class="newsletter-form mc_embed_signup">
-                            <form action="http://devitems.us11.list-manage.com/subscribe/post?u=6bbb9b6f5827bd842d9640c82&amp;id=05d85f18ef" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                            <form action="{{route('contacts.index')}}" >
                                 <div id="mc_embed_signup_scroll" class="mc-form">
-                                    <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Enter your e-mail address" required>
+{{--                                    <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Enter your e-mail address" required>--}}
                                     <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                                    <div class="mc-news" aria-hidden="true"><input type="text" name="b_6bbb9b6f5827bd842d9640c82_05d85f18ef" tabindex="-1" value=""></div>
-                                    <button id="mc-embedded-subscribe" class="default-btn" type="submit" name="subscribe"><span>subscribe</span></button>
+{{--                                    <div class="mc-news" aria-hidden="true"><input type="text" name="b_6bbb9b6f5827bd842d9640c82_05d85f18ef" tabindex="-1" value=""></div>--}}
+                                    <div> <button <a href="{{route('contacts.index')}}" class="default-btn"></a> <span>Send us your query</span></button>
+                                    </div>
                                 </div>
                             </form>
+                        </div>
                             <!-- mailchimp-alerts Start -->
                             <div class="mailchimp-alerts">
                                 <div class="mailchimp-submitting"></div><!-- mailchimp-submitting end -->
@@ -350,7 +352,6 @@
                     </div>
                 </div>
             </div>
-        </div>
         <!-- Subscribe End -->
         <!-- Footer Start -->
 
